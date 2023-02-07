7eff7e39c0b8529f7d448ae92747b738ebc869b2
Art Frich <frich.g23@yandex.ru>
2023-02-08 01:16:21 +0500

    hotfixes: рефакторинг кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,46 @@
-var btnEdit = document.querySelector('.profile__btn-edit');
-var btnAdd = document.querySelector('.profile__btn-add');
-var btnSaveEdit = document.querySelector('.popup__btn-save-edit');
-var btnCloseEdit = document.querySelector('.popup__btn-close-edit');
+let btnEdit = document.querySelector('.profile__btn-edit');
+let btnCloseEdit = document.querySelector('.popup__btn-close-edit');
 
-var formElement = document.querySelector('.popup');
-var nameInput = formElement.querySelector('.popup__input-name');
-var jobInput = formElement.querySelector('.popup__input-about');
+let formPopup = document.querySelector('.popup__container')
+let popup = document.querySelector('.popup');
+let popupInput = popup.querySelectorAll('.popup__input');
+let nameInput = popupInput[0];
+let jobInput = popupInput[1];
 
-var profile = document.querySelector('.profile');
-var nameUser = profile.querySelector('.profile__title-name');
-var nameAbout = profile.querySelector('.profile__subtitle');
+let profile = document.querySelector('.profile');
+let nameUser = profile.querySelector('.profile__title-name');
+let nameAbout = profile.querySelector('.profile__subtitle');
 
-var popupOpen = 'popup_opened'
+let classPopupOpened = 'popup_opened'
 
-// кнопка изменения данных профиля
-btnEdit.addEventListener('click', function () {
-  nameInput.placeholder = nameUser.textContent;
-  jobInput.placeholder = nameAbout.textContent;
-  formElement.classList.add(popupOpen);
-})
+function openPopup () {
+  nameInput.value = nameUser.textContent;
+  jobInput.value = nameAbout.textContent;
+  popup.classList.add(classPopupOpened);
+}
 
-// кнопка "закрыть" окно изменения данных профиля
-btnCloseEdit.addEventListener('click', function () {
-  nameInput.value = null;
-  jobInput.value = null;
-  formElement.classList.remove(popupOpen);
-})
+function closePopup () {
+  popup.classList.remove(classPopupOpened);
+}
 
-// кнопка "сохранить" изменения данных профиля
-btnSaveEdit.addEventListener('click', function () {
-  nameUser.textContent = nameInput.value;
-  nameAbout.textContent = jobInput.value;
-  formElement.classList.remove(popupOpen);
-})
-
-
-function handleFormSubmit (evt) {
+function handleFormSubmit (event) {
   // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
-    evt.preventDefault(); 
+    event.preventDefault()    
 
     nameUser.textContent = nameInput.value;
     nameAbout.textContent = jobInput.value;
-    formElement.classList.remove(popupOpen);
+    popup.classList.remove(classPopupOpened);
 }
+
+
+// кнопка изменения данных профиля
+btnEdit.addEventListener('click', openPopup);
+
+// кнопка "закрыть" окно изменения данных профиля
+btnCloseEdit.addEventListener('click', closePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+formPopup.addEventListener('submit', handleFormSubmit);
