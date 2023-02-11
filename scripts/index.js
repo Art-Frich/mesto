b7b363f9c8fe1b0ae8d45e4c74aa9120a3d907ad
Art Frich <frich.g23@yandex.ru>
2023-02-11 19:30:05 +0500

    feat/create new place

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,35 @@
+// Кнопки
 const btnEdit = document.querySelector('.profile__btn-edit');
-const btnCloseEdit = document.querySelector('.popup__btn-close-edit');
+const btnAddPlace = document.querySelector('.profile__btn-add');
+const btnCloseEdit = document.querySelector('.popup__btn-close_type_profile');
+const btnClosePlace = document.querySelector('.popup__btn-close_type_place');
 
-const form = document.querySelector('.popup__form')
-const popup = document.querySelector('.popup');
-const nameInput = popup.querySelector('.popup__input_type_name');
-const aboutInput = popup.querySelector('.popup__input_type_about');
+// Формы
+const formAddPlace = document.querySelector('.popup__form[name="addNewPlace"]');
+const formEditProfile = 
+  document.querySelector('.popup__form[name="editProfileText"]');
 
+//попап профиля
+const popupEditProfile = document.querySelector('.popup_type_editProfile');
+const nameUserInput = 
+  popupEditProfile.querySelector('.popup__input_type_name-user');
+const aboutInput = popupEditProfile.querySelector('.popup__input_type_about');
+
+// попап места
+const popupAddPlace = document.querySelector('.popup_type_addPlace');
+const namePlaceInput = 
+  popupAddPlace.querySelector('.popup__input_type_name-place')
+const urlInput = popupAddPlace.querySelector('.popup__input_type_url');
+
+// профиль
 const profile = document.querySelector('.profile');
 const nameUser = profile.querySelector('.profile__title-name');
 const nameAbout = profile.querySelector('.profile__subtitle');
 
-const classPopupOpened = 'popup_opened'
+//служебная переменная
+const classPopupOpened = 'popup_opened';
 
+//сетка мест и массив мест
 const placesGrid = document.querySelector('.places__grid');
 const initialCards = [
   {
@@ -40,21 +58,24 @@ const initialCards = [
   }
 ];
 
-function openPopup () {
-  nameInput.value = nameUser.textContent;
-  aboutInput.value = nameAbout.textContent;
-  popup.classList.add(classPopupOpened);
+function openPopup (btn) {
+  btn.classList.add(classPopupOpened);
 }
 
+// закрываем попам и очищаем поля ввода
 function closePopup () {
+  let popup = document.querySelector('.popup_opened')
+  let input = popup.querySelectorAll('.popup__input');
+  input.forEach((item) => {
+    item.value = null;
+  })
   popup.classList.remove(classPopupOpened);
 }
 
+// просто закрывает попап
 function handleFormSubmit (event) {
   // Эта строчка отменяет стандартную отправку формы.
-    event.preventDefault()    
-    nameUser.textContent = nameInput.value;
-    nameAbout.textContent = aboutInput.value;
+    event.preventDefault();
     closePopup ();
 }
 
@@ -66,18 +87,39 @@ function addPlace(item) {
   placeElement.querySelector('.places__grid-item-photo').src = item.link;
   placeElement.querySelector('.places__grid-item-title').textContent = 
     item.name;
-  placesGrid.append(placeElement);
+  placesGrid.prepend(placeElement);
 }
 
 // загружаем стартовые значения
 initialCards.forEach(addPlace);
 
 // кнопка изменения данных профиля
-btnEdit.addEventListener('click', openPopup);
+btnEdit.addEventListener('click', () => {
+  nameUserInput.value = nameUser.textContent;
+  aboutInput.value = nameAbout.textContent;
+  openPopup(popupEditProfile);
+});
+
+// кнопка добавления нового места
+btnAddPlace.addEventListener('click', () => {
+  openPopup(popupAddPlace);
+});
 
 // кнопка "закрыть" окно изменения данных профиля
 btnCloseEdit.addEventListener('click', closePopup);
+btnClosePlace.addEventListener('click', closePopup);
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-form.addEventListener('submit', handleFormSubmit);
+formEditProfile.addEventListener('submit', (ev) => {
+  nameUser.textContent = nameUserInput.value;
+  nameAbout.textContent = aboutInput.value;
+  handleFormSubmit(ev);
+});
+
+formAddPlace.addEventListener('submit', (ev) => {
+  let lastIndex = initialCards.length - 1;
+  initialCards.push({});
+  initialCards[lastIndex].link = urlInput.value;
+  initialCards[lastIndex].name = namePlaceInput.value;
+  addPlace(initialCards[lastIndex]);
+  handleFormSubmit(ev);
+})
